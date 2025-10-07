230b4088178e10c42025485358b45a005dcc1e11
janan0927 <janan0927@outlook.com>
2025-10-07 13:42:49 +0800

    first commit

---
+++ iNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNoteApp.swift
+//  iNote
+//
+//  Created by janan0927.
+//
+
+import SwiftUI
+
+@main
+struct iNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
